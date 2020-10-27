481a7e5c6b714a1fab225d7d6b031b82a7dc4a9e
starseb-rgb <seb.basti3@web.de>
2020-10-27 13:43:39 +0100

    created model artists and table

--- src/app/add-artist/add-artist.component.ts
+++ src/app/add-artist/add-artist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Artist } from '../shared/artist.model';
 
 @Component({
   selector: 'app-add-artist',
@@ -7,16 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class AddArtistComponent implements OnInit {
 
   artistName: string;
-  artists: string[];
-
+  artists: Artist[] = [];
+  random: string[] = ['https://www.klatsch-tratsch.de/wp-content/uploads/2019/10/drake-2.jpg', 'https://media.resources.festicket.com/image/300x300/center/top/filters:quality(70)/www/artists/JCole_hX9yvF7.jpg', 'https://ais.rtl.de/masters/1269695/1686x0/QG4CPQ7VF6PR37L6EAO3IHMJ2I.jpg'];
   constructor() { }
 
   ngOnInit(): void {
   }
 
   addArtist(): void {
-    this.artists.push(this.artistName);
-    console.log(this.artists);
+    this.artists.push(
+      new Artist( this.artistName, this.random[Math.floor(Math.random() * 3)])
+    );
   }
 
 }
